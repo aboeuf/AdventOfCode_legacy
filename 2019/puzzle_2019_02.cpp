8aa20d81de024743aa952d3cda62a647b1db53ad
Alexandre Boeuf <alexandre.boeuf@easymile.com>
2025-01-03 16:08:07 +0100

    2019 Day 02

--- 2019/puzzle_2019_02.cpp
+++ 2019/puzzle_2019_02.cpp
@@ -1,6 +1,45 @@
+#include <2019/intcodecomputer.h>
 #include <2019/puzzle_2019_02.h>
 #include <common.h>
 
-void Solver_2019_02_1::solve(const QString &input) { emit finished(input); }
+namespace puzzle_2019_02 {
 
-void Solver_2019_02_2::solve(const QString &input) { emit finished(input); }
+using namespace event_2019;
+
+class GravityAssistProgram {
+public:
+  GravityAssistProgram(const QString &input)
+      : m_computer{common::splitLines(input, true).front()} {}
+
+  QString run(const Int first_input, const Int second_input) {
+    m_computer.reset();
+    m_computer.set(1, first_input);
+    m_computer.set(2, second_input);
+    m_computer.run();
+    return QString("%1").arg(m_computer.state().get(0));
+  }
+
+  QString solve() {
+    for (auto noun = Int(0); noun < Int(100); ++noun) {
+      for (auto verb = Int(0); verb < Int(100); ++verb) {
+        if (run(noun, verb) == "19690720") {
+          return QString("%1").arg(Int(100) * noun + verb);
+        }
+      }
+    }
+    return "Failure";
+  }
+
+private:
+  IntcodeComputer m_computer;
+};
+
+} // namespace puzzle_2019_02
+
+void Solver_2019_02_1::solve(const QString &input) {
+  emit finished(puzzle_2019_02::GravityAssistProgram(input).run(12, 2));
+}
+
+void Solver_2019_02_2::solve(const QString &input) {
+  emit finished(puzzle_2019_02::GravityAssistProgram(input).solve());
+}
--- solvers.h
+++ solvers.h
@@ -1,30 +1,27 @@
 #pragma once
 
 #include <QMap>
-#include <QString>
 #include <QObject>
-#include <2019/intcodecomputer.h>
+#include <QString>
 #include <display/display.h>
 
-class Solver : public QObject
-{
+class Solver : public QObject {
   Q_OBJECT
 
 signals:
-  void finished(const QString& output);
-  void output(const QString& data);
+  void finished(const QString &output);
+  void output(const QString &data);
 
 public:
   virtual ~Solver() {}
-  virtual void solve(const QString& input) = 0;
-  Display* m_display{nullptr};
+  virtual void solve(const QString &input) = 0;
+  Display *m_display{nullptr};
 };
 
-struct Solvers
-{
+struct Solvers {
   Solvers();
   ~Solvers();
 
-  Solver* operator()(int year, int day, int puzzle) const;
-  QMap<int, QMap<int, QMap<int, Solver*>>> m_solvers;
+  Solver *operator()(int year, int day, int puzzle) const;
+  QMap<int, QMap<int, QMap<int, Solver *>>> m_solvers;
 };
